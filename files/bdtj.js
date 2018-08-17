dde67da484dcd8024d24b09593439eee1269b9c1
long <357225027@qq.com>
2018-08-17 16:24:09 +0800

    Add files via upload

---
+++ files/bdtj.js
@@ -0,0 +1,2 @@
+﻿var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F5deebda9c0777c0a5c14f52977ebbdbc' type='text/javascript'%3E%3C/script%3E"));
